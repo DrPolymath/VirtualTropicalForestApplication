cfa8c17a5c0d829d5a3710b3996f6d67925d189b
AyeDee25 <63199919+AyeDee25@users.noreply.github.com>
2022-01-13 22:48:26 +0800

    Update Lightning.java

--- src/main/java/app/virtualtropicalforestapplication/Lightning.java
+++ src/main/java/app/virtualtropicalforestapplication/Lightning.java
@@ -2,8 +2,6 @@ package app.virtualtropicalforestapplication;
 
 public class Lightning extends WeatherDecorator {
 
-    Weather weather;
-
     public Lightning(Weather weather) {
         this.weather = weather;
     }
