15242f9b9988be17a7cc509ee9a08b3c1624a531
AyeDee25 <63199919+AyeDee25@users.noreply.github.com>
2022-01-13 22:48:00 +0800

    Update Windy.java

--- src/main/java/app/virtualtropicalforestapplication/Windy.java
+++ src/main/java/app/virtualtropicalforestapplication/Windy.java
@@ -2,8 +2,6 @@ package app.virtualtropicalforestapplication;
 
 public class Windy extends WeatherDecorator {
 
-    Weather weather;
-
     public Windy(Weather weather) {
         this.weather = weather;
     }
