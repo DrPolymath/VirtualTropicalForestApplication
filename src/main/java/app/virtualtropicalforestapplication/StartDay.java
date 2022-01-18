605674b70398d1972c30c43fe62b4f48f080469d
GeekDog99 <62506545+GeekDog99@users.noreply.github.com>
2022-01-18 16:23:59 +0800

    Update StartDay.java

--- src/main/java/app/virtualtropicalforestapplication/StartDay.java
+++ src/main/java/app/virtualtropicalforestapplication/StartDay.java
@@ -1,6 +1,12 @@
 package app.virtualtropicalforestapplication;
 
 public class StartDay implements State{
+    public Context context;
+
+    public StartDay(Context context) {
+        this.context = context;
+    }
+    
     @Override
     public void change(Context context) {
         System.out.println("Forest at day.");
