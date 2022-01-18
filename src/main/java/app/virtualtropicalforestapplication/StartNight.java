47ab48b3b3cf580277724fddf5acc29f66a3c803
GeekDog99 <62506545+GeekDog99@users.noreply.github.com>
2022-01-18 16:24:27 +0800

    Update StartNight.java

--- src/main/java/app/virtualtropicalforestapplication/StartNight.java
+++ src/main/java/app/virtualtropicalforestapplication/StartNight.java
@@ -1,6 +1,12 @@
 package app.virtualtropicalforestapplication;
 
 public class StartNight implements State{
+    public Context context;
+
+    public StartNight(Context context) {
+        this.context = context;
+    }
+    
     @Override
     public void change(Context context) {
         System.out.println("Forest at night.");
